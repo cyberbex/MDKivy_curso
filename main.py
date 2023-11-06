efe273aa3e4c35a0c8088262c340548e4c58578f
Bruno Migliorini <cyberbex@gmail.com>
2023-11-06 13:02:08 +0000

    banners

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
 
 Window.size=(480,800)
-class MI(MDBoxLayout):
-    pass
+class MI(Screen):
+    def lefter(self,Instance):
+        print("Left pressed",Instance.text)
+    
+    def righter(self,Instance):
+        print("Right pressed",Instance.text)
+
 class MyApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette="Amber"
